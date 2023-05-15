0ed81f1fef86ce664f1b61405a6c6a06358f7a5f
Darius Tanz <scratchticketseb@gmail.com>
2023-05-15 14:42:04 +0000

    May 15, 2023, 10:42 AM

--- FrontEnd/src/JS/interaction.js
+++ FrontEnd/src/JS/interaction.js
@@ -1,7 +1,7 @@
 
 const contractAddress = "0xf5c9e57e177B4F5CCfCb13b18e4154774E917401";
 const ABI = window.abi;
-const Chainlist
+const Chainlist = window.Chainlist;
 let account;
 let netID;
 let LoggedIn = false;
