4f6984924c745ebce6a5b2ab16c759e3fe6c652d
Darius Tanz <scratchticketseb@gmail.com>
2023-05-15 16:06:46 +0000

    May 15, 2023, 12:06 PM

--- FrontEnd/src/JS/interaction.js
+++ FrontEnd/src/JS/interaction.js
@@ -16,6 +16,7 @@ async function loginWithEth(){
         await ethereum.request({ method: 'eth_requestAccounts' });
         window.web3 = await new Web3(ethereum);
         await getID();
+        SwitchChainHeader();
         accountarray = await web3.eth.getAccounts();
         contract = new window.web3.eth.Contract(ABI, contractAddress, window.web3);
         account = accountarray[0];
