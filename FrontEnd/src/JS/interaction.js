4d939a9cda3fabcdb57907fad2d45c26f6ef96e8
Darius Tanz <scratchticketseb@gmail.com>
2023-05-24 13:45:52 +0000

    May 24, 2023, 9:45 AM

--- FrontEnd/src/JS/interaction.js
+++ FrontEnd/src/JS/interaction.js
@@ -63,7 +63,7 @@ async function SwitchChainHeader(){
 }
 
 async function DisplayBoxes(){
-    if(CollectionInput.innerText != ""){
+    if(CollectionInput.value != ""){
         Boxes.style.display = "";
     }
     else{
