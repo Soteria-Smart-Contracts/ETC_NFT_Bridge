67b887097cf23794723614e7d17143120d35bce7
Darius Tanz <scratchticketseb@gmail.com>
2023-06-05 18:27:32 +0000

    Jun 5, 2023, 2:27 PM

--- FrontEnd/src/JS/interaction.js
+++ FrontEnd/src/JS/interaction.js
@@ -97,7 +97,7 @@ async function Searching(){
         Boxes.style.display = "";
         Results = await SearchListings(CollectionInput.value);
         if(Results.length > 0){
-            let index;
+            let index = 0;
             let Output;
             while(index < Results.length){
                 Output = Output + "hello";
