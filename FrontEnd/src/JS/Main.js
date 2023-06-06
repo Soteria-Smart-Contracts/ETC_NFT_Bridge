017000fb013ac32913eb371309e5cf2d788b2d48
Darius Tanz <scratchticketseb@gmail.com>
2023-06-06 13:04:05 +0000

    Jun 6, 2023, 9:04 AM

--- FrontEnd/src/JS/Main.js
+++ FrontEnd/src/JS/Main.js
@@ -117,6 +117,7 @@ async function Unselect(){
     ReturnBTN.style.display = "none";
     SelectedHeader.innerHTML = '';
     DappHeader.innerText = "Select a collection to bridge, or add a new one";
+    CollectionInput.value = ''
 }
 
 async function Search(){
