19d567f1035e3b8feb1b0428d31b4e76ac256617
Darius Tanz <scratchticketseb@gmail.com>
2023-06-07 14:28:55 +0000

    Jun 7, 2023, 10:28 AM

--- FrontEnd/src/JS/Main.js
+++ FrontEnd/src/JS/Main.js
@@ -111,6 +111,7 @@ async function SwitchChainHeader(){
 async function Selected(Collection){
 
     if(Collection.includes("0x") && Collection.length == 42){
+        console.log(Collection);
     }
     
     CollectionSelected = true;
