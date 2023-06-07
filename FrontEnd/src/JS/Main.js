d480f0cf59a8fc930db9bc471d9a933e1dd36f64
Darius Tanz <scratchticketseb@gmail.com>
2023-06-07 14:37:23 +0000

    Jun 7, 2023, 10:37 AM

--- FrontEnd/src/JS/Main.js
+++ FrontEnd/src/JS/Main.js
@@ -147,7 +147,7 @@ async function Search(){
     if(CollectionInput.value != ""){
         Boxes.style.display = "";
         if(CollectionInput.value.includes("0x") && CollectionInput.value.length == 42){
-            Boxes.innerHTML = ("<a onclick='Selected(" + CollectionInput.value + ")'>" + "ERC721 at address " + CollectionInput.value + "</a>");
+            Boxes.innerHTML = ("<a onclick='Selected(" + CollectionInput.value) + ")'>" + "ERC721 at address " + CollectionInput.value + "</a>");
         }
         else{
             Results = await SearchListings(CollectionInput.value);
