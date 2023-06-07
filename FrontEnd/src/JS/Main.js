dadf1870bdbbc6c7cb5bf24f8e36f7e18f922f20
Darius Tanz <scratchticketseb@gmail.com>
2023-06-07 14:14:53 +0000

    Jun 7, 2023, 10:14 AM

--- FrontEnd/src/JS/Main.js
+++ FrontEnd/src/JS/Main.js
@@ -3,7 +3,7 @@ const BridgeAddress = "0xf5c9e57e177B4F5CCfCb13b18e4154774E917401";
 const BridgeABI = window.BridgeABI;
 const Chainlist = window.Chainlist;
 let BridgeContract;
-let 
+let NFT
 let account;
 let Results;
 let netID;
