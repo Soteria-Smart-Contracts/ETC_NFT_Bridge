62c2c66b2939eb45f8545b347df97841272f8810
Darius Tanz <scratchticketseb@gmail.com>
2023-06-07 14:30:33 +0000

    Jun 7, 2023, 10:30 AM

--- FrontEnd/src/JS/Main.js
+++ FrontEnd/src/JS/Main.js
@@ -112,7 +112,7 @@ async function Selected(Collection){
     if(Collection.includes("0x") && Collection.length == 42){
         NFTContract = new window.web3.eth.Contract(ERC721abi, Collection.Address, window.web3);
     }
-    else{
+   
         CollectionSelected = true;
         Searchbar.style.display = "none";
         ReturnBTN.style.display = "";
