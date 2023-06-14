6edbe3d57665b1aa520df6a78ceb50cd32ff4a49
Darius Tanz <scratchticketseb@gmail.com>
2023-06-14 20:27:28 +0000

    Jun 14, 2023, 4:27 PM

--- FrontEnd/src/JS/Main.js
+++ FrontEnd/src/JS/Main.js
@@ -71,7 +71,7 @@ async function loginWithEth(bypass){
             SwitchChainHeader();
             let accountarray = await web3.eth.getAccounts();
             BridgeContract = new window.web3.eth.Contract(BridgeABI, BridgeAddress, window.web3);
-             account = accountarray[0];
+            account = accountarray[0];
             console.log('Logged In')
             LoggedIn = true;
         } else { 
