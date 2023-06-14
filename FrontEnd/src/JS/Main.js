4d7476631838495aa99454771cb051ad13ddc4b5
Darius Tanz <scratchticketseb@gmail.com>
2023-06-14 20:21:55 +0000

    Jun 14, 2023, 4:21 PM

--- FrontEnd/src/JS/Main.js
+++ FrontEnd/src/JS/Main.js
@@ -133,7 +133,7 @@ async function Selected(Collection, New){
 
 async function IsOwner(){
     ID = parseInt(IDinput.value);
-    console.log(await NFTContract.methods.ownerOf(ID).call()
+    console.log(await NFTContract.methods.ownerOf(ID).call())
     if(await NFTContract.methods.ownerOf(ID).call() == account){
         console.log("This fucker owns that NFT");
         return(true)
