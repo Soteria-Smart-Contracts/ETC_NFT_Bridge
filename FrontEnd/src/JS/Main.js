e8fa7cc51049999e55b55ddbaf27a25c92eccbd3
Darius Tanz <scratchticketseb@gmail.com>
2023-06-18 21:56:04 +0000

    Jun 18, 2023, 5:56 PM

--- FrontEnd/src/JS/Main.js
+++ FrontEnd/src/JS/Main.js
@@ -122,7 +122,7 @@ async function SwitchChainHeader(){
 }
 
 async function Selected(Collection, New){
-    if(New == true){ //
+    if(New == true){ //TODO: Check to see if its ERC721 compatible using 
         Address = String(Collection);
         NFTContract = new window.web3.eth.Contract(ERC721abi, Address, window.web3);
         Collection = {"Name":"", "Address":""}
