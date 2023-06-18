fcbf68c580d3d2700d49049bacfbd76630cd5f45
Darius Tanz <scratchticketseb@gmail.com>
2023-06-18 21:52:13 +0000

    Jun 18, 2023, 5:52 PM

--- FrontEnd/src/JS/Main.js
+++ FrontEnd/src/JS/Main.js
@@ -100,7 +100,6 @@ async function getID(){
 async function SwitchChainHeader(){
     if(netID === 5){
         ChainText.innerText = " Goerli - ID 5"
-        
     }
     else if(netID === 61){
         ChainText.innerText = " Ethereum Classic - ID 61"
