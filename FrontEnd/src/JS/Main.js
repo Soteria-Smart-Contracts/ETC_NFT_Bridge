f9a0324755f94018ba161e35dfe932504234dcf3
Darius Tanz <scratchticketseb@gmail.com>
2023-06-18 23:22:16 +0000

    Jun 18, 2023, 7:22 PM

--- FrontEnd/src/JS/Main.js
+++ FrontEnd/src/JS/Main.js
@@ -255,8 +255,8 @@ async function Confirm(){
     if(!(await NFTContract.methods.isApprovedForAll(account, BridgeAddress).call())){
         gas = await NFTContract.methods.setApprovalForAll(BridgeAddress, true).estimateGas({from: account});
         Transactions.innerText = "Transaction 1 of 2 (Bridge approval on NFT contract)"
-        gas = await BridgeContract.methods.BridgeSend(NFTContract._address, ).estimateGas({from: account});
         TX = await Bridge.methods.setApprovalForAll(BridgeAddress, true).send({from: account, value: 0, gas: gas})
+        gas = await BridgeContract.methods.BridgeSend(NFTContract._address, ).estimateGas({from: account});
         Transactions.innerText = "Transaction 2 of 2 (Bridge approval on NFT contract)"
         ID = parseInt(IDinput.value);
         
