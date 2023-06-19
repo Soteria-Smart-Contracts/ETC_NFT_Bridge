3b2855c9c3d85348d9154b9dac7d8a556597a9f0
Darius Tanz <scratchticketseb@gmail.com>
2023-06-19 00:02:51 +0000

    Jun 18, 2023, 8:02 PM

--- FrontEnd/src/JS/Main.js
+++ FrontEnd/src/JS/Main.js
@@ -266,10 +266,9 @@ async function Confirm(){
     else{
         Transactions.innerText = "Transaction 1 of 1 (Bridge Request)";
         ID = parseInt(IDinput.value);
-        console.log(ID);
+        value = 500000000000000000;
         gas = await BridgeContract.methods.BridgeSend(NFTContract._address, ID, SelectedDest).estimateGas({from: account, value: value});
         //TODO:Setup multichain gas calculator based on their smart contracts for mainnet
-        value = 500000000000000000;
         await BridgeContract.methods.BridgeSend(NFTContract._address, ID, SelectedDest).send({from: account, value: value, gas: gas});
     }
     
