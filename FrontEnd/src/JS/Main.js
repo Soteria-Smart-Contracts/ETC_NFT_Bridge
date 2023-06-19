fce40afb7159f3908ec79859e63f074a1d184d91
Darius Tanz <scratchticketseb@gmail.com>
2023-06-19 16:55:56 +0000

    Jun 19, 2023, 12:55 PM

--- FrontEnd/src/JS/Main.js
+++ FrontEnd/src/JS/Main.js
@@ -137,7 +137,7 @@ async function Selected(Collection, New){
         try {
             Collection.Name = await NFTContract.methods.name().call()
         } catch (error) {
-            IDinputRes.innerText = "Unable to locate ERC721 contract at address" + Address;
+            IDinputRes.innerText = "Unable to locate ERC721 contract at address " + Address;
             return;
         };
     }
